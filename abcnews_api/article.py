79566e565c5b60e38ab3fc19532c3423b2e075f7
king-millez <Millez.Dev@gmail.com>
2020-10-09 23:36:33 +1100

    Added support for articles with multiple authors

--- abcnews_api/article.py
+++ abcnews_api/article.py
@@ -5,7 +5,7 @@ from requests import get
 ID = 'id'
 TITLE = 'title'
 TEASER_TITLE = 'teaser_title'
-AUTHOR = 'author'
+AUTHORS = 'authors'
 PUBLISHED_TIME = 'published_time'
 MODIFIED_TIME = 'modified_time'
 CATEGORY = 'category'
@@ -15,8 +15,7 @@ URL = 'url'
 
 def read_article(url):
     if not re.match(r'https?://(?:www\.)?abc\.net\.au/news/(?:[^/]+/){1,2}(?P<id>\d+)', url):
-        print('Invalid URL')
-        exit
+        return('Invalid URL...')
 
     _article_url = url
     _article_id = url[-8:]
@@ -35,9 +34,12 @@ def read_article(url):
         _article_category = 'N/A'
     
     try:
-        _article_author = soup.find('a', {'class': '_2f8qj FQVx7 _2tPjN _1QHxY z7Xgw'}).text
-    except:
-        _article_author = 'N/A'
+        _article_authors = []
+        for author in soup.findAll('a', {'class': '_2f8qj FQVx7 _2tPjN _1QHxY z7Xgw'}):
+            _article_authors.append(author.text)
 
-    _article_dict = {URL: _article_url, ID: _article_id, TITLE: _article_title, TEASER_TITLE: _article_teaser, AUTHOR: _article_author, PUBLISHED_TIME: _article_published_time, MODIFIED_TIME: _article_modified_time, CATEGORY: _article_category}
+    except:
+        _article_authors = ['N/A']
+
+    _article_dict = {URL: _article_url, ID: _article_id, TITLE: _article_title, TEASER_TITLE: _article_teaser, AUTHORS: _article_authors, PUBLISHED_TIME: _article_published_time, MODIFIED_TIME: _article_modified_time, CATEGORY: _article_category}
     return(_article_dict)
