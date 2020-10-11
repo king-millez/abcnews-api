9c862f832822a43ed38cfcdf4e8736e0f98d7e20
king-millez <Millez.Dev@gmail.com>
2020-10-11 13:56:56 +1100

    Fixed some articles' authors not being detected

--- abcnews_api/article.py
+++ abcnews_api/article.py
@@ -26,20 +26,32 @@ def read_article(url):
     soup = BeautifulSoup(page, 'html.parser')
     _article_title = soup.find('title').text[:-11]
     _article_teaser = soup.find('meta', {'property': 'og:title'})['content']
-    _article_published_time = soup.find('meta', {'property': 'article:published_time'})['content']
-    _article_modified_time = soup.find('meta', {'property': 'article:modified_time'})['content']
+    try:
+        _article_published_time = soup.find('meta', {'property': 'article:published_time'})['content']
+    except:
+        _article_published_time = soup.find('meta', {'name': 'DCTERMS.issued'})['content']
+    
+    try:
+        _article_modified_time = soup.find('meta', {'property': 'article:modified_time'})['content']
+    except:
+        _article_modified_time = soup.find('meta', {'name': 'DCTERMS.modified'})['content']
+    
     try:
         _article_category = soup.find('a', {'class': '_1v60e _2f8qj FQVx7 _2tPjN _1QHxY'}).text
     except:
-        _article_category = 'N/A'
+        _article_category = 'none'
     
     try:
         _article_authors = []
-        for author in soup.findAll('a', {'class': '_2f8qj FQVx7 _2tPjN _1QHxY z7Xgw'}):
-            _article_authors.append(author.text)
+        author_span = soup.find_all('span', {'class': '_21SmZ _3_Aqg _1hGzz _1-RZJ P8HGV', 'data-component': 'Text'})[len(soup.find_all('span', {'class': '_21SmZ _3_Aqg _1hGzz _1-RZJ P8HGV', 'data-component': 'Text'})) - 1]
+        for tag in author_span.find_all('a', {'class', '_2f8qj FQVx7 _2tPjN _1QHxY z7Xgw'}):
+            if(tag['href'][:28] != 'https://www.abc.net.au/news/'):
+                _article_authors = [author_span.text.split(' ')[1] + ' ' + author_span.text.split(' ')[2]]
+            else:
+                _article_authors.append(tag.text)
 
     except:
-        _article_authors = ['N/A']
+        print('oh')
 
     _article_dict = {URL: _article_url, ID: _article_id, TITLE: _article_title, TEASER_TITLE: _article_teaser, AUTHORS: _article_authors, PUBLISHED_TIME: _article_published_time, MODIFIED_TIME: _article_modified_time, CATEGORY: _article_category}
     return(_article_dict)
